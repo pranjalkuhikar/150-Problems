56e83adb78d0e38999f3bd7c196d5b8c8ca9e658
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2024-12-31 19:08:07 +0530

    add PrimeNumber function to determine if a number is prime

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const prompt = require("prompt-sync")();
 
 // Question 1 (Even Odd Number {8})
-function EvenOdd(n) {
-  if (n / 2 === 0) return "even";
-  else return "odd";
+// function EvenOdd(n) {
+//   if (n / 2 === 0) return "even";
+//   else return "odd";
+// }
+// console.log(EvenOdd(1));
+
+// Question 2 (Prime Number {7})
+function PrimeNumber(n) {
+  let count = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      count++;
+      if (n / i != i) {
+        count++;
+      }
+    }
+  }
+  return count === 2 ? "Prime Number" : "Not a Prime Number";
 }
-console.log(EvenOdd(1));
+console.log(PrimeNumber(1));
