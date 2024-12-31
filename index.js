0db73cb52ffd5899d4e932b5f852288867c7fd24
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2024-12-31 19:07:16 +0530

    add EvenOdd function to determine if a number is even or odd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const prompt = require("prompt-sync")();
+
+// Question 1 (Even Odd Number {8})
+function EvenOdd(n) {
+  if (n / 2 === 0) return "even";
+  else return "odd";
+}
+console.log(EvenOdd(1));
